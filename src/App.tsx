fb6c9ad82d30216498c3d3f54c0997e7c6892b26
Robert Adams <adamsiii.robert@gmail.com>
2022-11-01 19:38:30 -0700

    redeploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,8 @@
-import {
-  Box,
-  ChakraProvider,
-  Flex,
-  FormLabel,
-  Slider,
-  SliderFilledTrack,
-  SliderThumb,
-  SliderTrack,
-  Switch,
-  useBoolean,
-} from "@chakra-ui/react";
+import { ChakraProvider, Flex } from "@chakra-ui/react";
 import { theme } from "./theme";
 import { PillarLogo } from "./components/PillarLogo";
-import { useState } from "react";
 
 const App = () => {
-  const [size, setSize] = useState(256);
-  const [loader, setLoader] = useBoolean(false);
-  const [altColors, setAltColors] = useBoolean(false);
-  const [withBackground, setWithBackground] = useBoolean(true);
-  const [loopDuration, setLoopDuration] = useState(1);
-  const [loopDelay, setLoopDelay] = useState(0);
-
   return (
     <ChakraProvider theme={theme}>
       <Flex
@@ -32,73 +13,22 @@ const App = () => {
         backgroundColor="white"
       >
         <Flex bgColor="gray.100" p="lg" borderRadius="3xl">
-          <Box h="400" w="400" px="xl">
-            <FormLabel>animated</FormLabel>
-            <Switch size="lg" isChecked={loader} onChange={setLoader.toggle} />
-
-            <FormLabel>use background</FormLabel>
-            <Switch
-              size="lg"
-              isChecked={withBackground}
-              onChange={setWithBackground.toggle}
-            />
-
-            <FormLabel>alternate colors</FormLabel>
-            <Switch
-              size="lg"
-              isChecked={altColors}
-              onChange={setAltColors.toggle}
-            />
-
-            <FormLabel>logo size</FormLabel>
-            <Slider
-              value={size}
-              min={48}
-              max={256}
-              onChangeEnd={val => setSize(val)}
-            >
-              <SliderTrack>
-                <SliderFilledTrack />
-              </SliderTrack>
-              <SliderThumb />
-            </Slider>
-
-            <FormLabel>loop duration</FormLabel>
-            <Slider
-              value={loopDuration}
-              min={0.5}
-              max={5}
-              step={0.5}
-              onChange={val => setLoopDuration(val)}
-            >
-              <SliderTrack>
-                <SliderFilledTrack />
-              </SliderTrack>
-              <SliderThumb />
-            </Slider>
-
-            <FormLabel>loop delay</FormLabel>
-            <Slider
-              value={loopDelay}
-              min={0}
-              max={5}
-              step={0.25}
-              onChange={val => setLoopDelay(val)}
-            >
-              <SliderTrack>
-                <SliderFilledTrack />
-              </SliderTrack>
-              <SliderThumb />
-            </Slider>
-          </Box>
-          <Flex w="300px" align="center" justify="center">
+          <Flex w="600px" align="center" justify="space-evenly">
             <PillarLogo
-              size={size}
-              loader={loader}
-              altColors={altColors}
-              withBackground={withBackground}
-              loopDuration={loopDuration}
-              loopDelay={loopDelay}
+              size={256}
+              loader
+              altColors
+              withBackground
+              loopDuration={1}
+              loopDelay={0}
+            />
+
+            <PillarLogo
+              size={256}
+              loader
+              withBackground
+              loopDuration={1}
+              loopDelay={0}
             />
           </Flex>
         </Flex>
